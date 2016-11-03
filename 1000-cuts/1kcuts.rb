b8c090c2f44da957e7b422cb000b582a434d718e
Peter <pwhite00@aol.com>
2016-11-02 18:27:38 -0700

    fleshing out db methods in the script ( first pass)

--- 1000-cuts/1kcuts.rb
+++ 1000-cuts/1kcuts.rb
@@ -9,12 +9,22 @@ end
 
 require 'optparse'
 require 'sqlite3'
+require 'fileutils'
 
 # database lives @ env['Home']/.1kc
 
 # create an alias in .bashrc to display a current count
 
-@options = { :version => 1.0, :import => nil, :note => nil, :mode => :status, :verbose => false, :db => '.1kc.db' }
+@options = { :version => 1.0,
+             :import => nil,
+             :note => nil,
+             :mode => 'status',
+             :verbose => false,
+             :db => "#{env['HOME']}/.ikcdb/ikc.db",
+             :dbPath => "#{env['HOME']}/.kcdb",
+             :value  => 1 ,
+}
+
 @modes = ['new', 'status', 'append']
 parser = OptionParser.new do |opts|
   opts.banner = "Usage: #{$0} [args]"
@@ -36,6 +46,9 @@ parser = OptionParser.new do |opts|
   opts.on('-v', '--verbose', 'display a more verbose output.') do
     @options[:verbose] = true
   end
+  opts.on('-o', '--override-value VALUE', 'If a cut warrents it add a number value [defaults to 1].') do |x|
+    @options[:value] = x
+  end
 end
 parser.parse!
 
@@ -44,6 +57,13 @@ def getStatus(isVerbose)
   printf("getStatus\n")
   # read current count and report
   # if verbose report more
+  unless File.exists?(@options[:db])
+    printf("Unable to locate database. Clearly this is just one more cut...\n")
+    exit 67
+  end
+  db = SQLite3::Database.new(@options[:db])
+  count = db.execute("select value from cuts;")
+  printf("Current cuts [#{count}]\n")
   exit
 end
 
@@ -51,14 +71,31 @@ def setupDb(importDb, isVerbose)
   # importDB is @options[:import]
   printf("setup\n")
   # check if the file exists
-  file_is_there = File.exists?(#add a db file to check)
-  
-  
-  # if verbose report more
-  # check if import is on and if import file exists ?
-  # if not importing create blank db
-  # if importing import
+  file_is_there = File.exists?(:db)
+  unless @options[:importDb].nil?
+    if file_is_there
+      printf(" - Unable to import database [#{@options[:importDb]}] database already exists." )
+      exit 1
+    else
+      unless Dir.exist?(@options[:dbPath])
+        FileUtils.mkdir_p(@options[:dbPath], mode => 0755)
+      end
+      FileUtils.cp(@options[:importDb], @options[:db])
+      # print a message
+    end
+  else
+    # make the db
+    mkFreshDb
+  end
   exit
+end
+
+def makeFreshDb()
+  # make a sqlite db
+  # schema:  CREATE TABLE cuts (value Integer, timestamp TEXT, comment TEXT)
+  db = SQLite3::Database.new(@options[:db])
+  db.execute("CREATE TABLE cuts (value Integer, timestamp TEXT, comment TEXT)")
+  printf("Created a new database [#{@options[:db]}\n")
 end
 
 def addEntry(addMsg, isVerbose)
@@ -67,6 +104,17 @@ def addEntry(addMsg, isVerbose)
   # check for message
   # if verbose report more
   # add tally with message if included.
+  unless File.exists?(@options[:db])
+    printf("Unable to locate database. Clearly this is just one more cut...\n")
+    exit 67
+  end
+  db = SQLite3::Database.new(@options[:db])
+  unless @options[:note].nil?
+    db.execute("INSERT INTO cuts (value, timestamp, comment) VALUES (#{@options[:value]}, #{timestamp}, #{@options[:note]})")
+  else
+    db.execute("INSERT INTO cuts (value, timestamp, comment) VALUES (#{@options[:value]}, #{timestamp}, #{@options[:note]})")
+  end
+  printf("a #{value} point cut has been registered.\n")
   getStatus
   exit
 end
